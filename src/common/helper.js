a9eadbe3a8abbbba1359fdaaf6696c0dd273a6fb
sachin-maheshwari <sachin.maheshwari@topcoder.com>
2019-02-04 18:26:33 +0530

    Update helper.js
    reverting my changes for excess call, fixing in core lib.

--- src/common/helper.js
+++ src/common/helper.js
@@ -2,16 +2,13 @@
 const m2mAuth = require('tc-core-library-js').auth.m2m
 const request = require('superagent')
 const _ = require('lodash')
-var m2m = null
+const m2m = m2mAuth(_.pick(config, ['AUTH0_URL', 'AUTH0_AUDIENCE', 'TOKEN_CACHE_TIME', 'AUTH0_PROXY_SERVER_URL']))
 
 /*
  * Function to get M2M token
  * @returns {Promise}
  */
 const getM2Mtoken = async (config) => {
-  if (_.isNull(m2m)) {
-    m2m = m2mAuth(_.pick(config, ['AUTH0_URL', 'AUTH0_AUDIENCE', 'TOKEN_CACHE_TIME', 'AUTH0_PROXY_SERVER_URL']))
-  }
   return m2m.getMachineToken(config.AUTH0_CLIENT_ID, config.AUTH0_CLIENT_SECRET)
 }
 
